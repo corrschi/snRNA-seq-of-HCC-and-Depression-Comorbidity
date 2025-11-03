d6d9b1c6ba6dda5198666e1857e1d00d039a1ba3
corrschi <54493785+corrschi@users.noreply.github.com>
2025-11-03 09:30:44 +0800

    Refactor DoubletFinder and Seurat object merging
    Refactor DoubletFinder workflow and update Seurat object merging process. Added QC summary generation and improved metadata handling.

--- 02.doubletFinder.R
+++ 02.doubletFinder.R
@@ -33,21 +33,21 @@ library(irlba)
 library(Nebulosa)
 
 ##### --------- part1.Doubletfinder ---------#####
-#参数设置
+# Parameter Set
 sample_list <- c("Stress_1","Stress_2","Stress_3","Control_1","Control_2","Control_3")
 res.use <- 0.5
 dim.use <- 30
 seed.use <- 88
 project.name <- "mdd.liver"
 
-# 路径设置
-data_dir_base <- "/public/home/chidm/Workspace/ouyang/06_mdd.liver/公司/singlesample"
-out_dir <- "/public/home/chidm/Workspace/ouyang/06_mdd.liver/paper/doublet-new"
+# path set
+data_dir_base <- "/public/home/chidm/Workspace/06_mdd.liver/singlesample"
+out_dir <- "/public/home/chidm/Workspace/06_mdd.liver/paper/doublet-new"
 setwd(out_dir)
 qc_plot_dir <- file.path(out_dir, "qc_plots")
 dir.create(qc_plot_dir, recursive = TRUE, showWarnings = FALSE)
 
-# doublet rate设置（参照doubletfinder官网表格）
+# doublet rate set（reference: doubletfinder）
 get_doublet_rate <- function(cell_number) {
   rate_table <- data.frame(
     rate = c(0.004, 0.008, 0.016, 0.023, 0.031, 0.039, 0.046, 0.054, 0.061, 0.069, 0.076),
@@ -65,23 +65,23 @@ for (i in sample_list) {
   obj <- CreateSeuratObject(counts = count.data,min.cells = 3,min.features = 200, project = project.name)
   obj[["percent.mt"]] <- PercentageFeatureSet(obj, pattern = "^mt-")
   
-  # QC图：小提琴图
+  # QC plot：Vlnplot
   png(filename = file.path(qc_plot_dir, paste0(i, "_before_filter_violin.png")), width = 10, height = 4, units = "in", res = 400)
   p1 <- VlnPlot(obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, pt.size = 0.1) + plot_layout(guides = "collect")
   print(p1)
   dev.off()
   
-  # 记录过滤前细胞数
+  # record  cell number before filtration 
   cells_before <- ncol(obj)
   
-  # 过滤低质量细胞
+  # filter low-quality cells
   obj <- subset(obj, subset =
                   nFeature_RNA >= 200 & nFeature_RNA <= 10000 & #不过滤nCount了
                   percent.mt <= 25)
   
   cells_after_qc <- ncol(obj)
   
-  # 标准流程
+  # standard pipline
   obj <- NormalizeData(obj)
   obj <- FindVariableFeatures(obj)
   obj <- ScaleData(obj)
@@ -90,7 +90,7 @@ for (i in sample_list) {
   obj <- FindClusters(obj, resolution = res.use)
   obj <- RunUMAP(obj, dims = 1:dim.use)
   
-  # DoubletFinder过程
+  # DoubletFinder
   sweep.res.list <- paramSweep(obj, PCs = 1:dim.use)
   sweep.stats <- summarizeSweep(sweep.res.list, GT = FALSE)
   
@@ -114,11 +114,11 @@ for (i in sample_list) {
   obj$DF_hi.lo <- ifelse(rowSums(hi_lo) == 2, "Singlet",
                          ifelse(rowSums(hi_lo) == 1, "Doublet_lo", "Doublet_hi"))
   
-  # 过滤掉 Doublet_hi，保留 Singlet 和 Doublet_lo
+  # filter Doublet_hi，keep Singlet and Doublet_lo
   obj <- subset(obj, subset = DF_hi.lo %in% c("Singlet", "Doublet_lo"))
   cells_after_doublet <- ncol(obj)
   
-  # 重新标准化 & 聚类
+  # Renormalization & clustering
   obj <- NormalizeData(obj)
   obj <- FindVariableFeatures(obj)
   obj <- ScaleData(obj, vars.to.regress = "nCount_RNA")
@@ -127,16 +127,16 @@ for (i in sample_list) {
   obj <- FindClusters(obj, resolution = res.use)
   obj <- RunUMAP(obj, dims = 1:dim.use)
   
-  # 保存 UMAP 图
+  # save UMAP plot
   png(paste0(out_dir, "/", i, "_UMAP_after_doublet_removal.png"), width = 6, height = 5, units = "in", res = 400)
   p <- DimPlot(obj, reduction = "umap", label = TRUE, pt.size = 0.3)
   print(p)
   dev.off()
   
-  # 保存处理后对象
+  # save object after doublet
   sample_list_after_doublet[[i]] <- obj
   
-  # 生成QC总表
+  # QC table
   qc_summary <- rbind(qc_summary, data.frame(
     sample = i,
     cells_before = cells_before,
@@ -145,37 +145,18 @@ for (i in sample_list) {
     estimated_doublet_rate = rate
   ))
 }
-# 保存QC总表
 write.csv(qc_summary, file = file.path(out_dir, "QC_summary_all_samples.csv"),  row.names = FALSE)
 
-##### -------- 合并 Seurat 对象 -------- ######
+##### -------- merge Seurat object -------- ######
 sample_order <- c("Stress_1", "Stress_2", "Stress_3", "Control_1", "Control_2", "Control_3")
 sample_list_doubletRM <- list()
-# ---- 重命名 barcode 并存入列表 ----
+## ---- rename barcode and save as list ----
 for (i in 1:length(sample_order)) {
   sample <- sample_order[i]
-  suffix <- i  # 用1–6作为后缀
+  suffix <- i  
   obj <- sample_list_after_doublet[[sample]]
-  # 改列名，加后缀
   new_colnames <- paste0(gsub("-1$", "", colnames(obj)), "-", suffix)
   colnames(obj) <- new_colnames
-  sample_list_doubletRM[[sample]] <- obj
-}
-
-### 2.1 合并去重后的 Seurat 对象，并统一处理 metadata
-sample_order <- c("Stress_1", "Stress_2", "Stress_3", "Control_1", "Control_2", "Control_3")
-sample_list_doubletRM <- list()
-
-## 重命名 barcode 加后缀
-for (i in 1:length(sample_order)) {
-  sample <- sample_order[i]
-  suffix <- i  # 用1–6作为后缀
-  obj <- sample_list_after_doublet[[sample]]
-  
-  # 修改列名，加后缀
-  new_colnames <- paste0(gsub("-1$", "", colnames(obj)), "-", suffix)
-  colnames(obj) <- new_colnames
-  
   sample_list_doubletRM[[sample]] <- obj
 }
 
@@ -190,7 +171,7 @@ merged_seurat <- merge(
 merged_seurat$sample <-paste0(str_split(colnames(merged_seurat),"_",simplify=T)[,1],"_",
                               str_split(colnames(merged_seurat),"_",simplify=T)[,2])
 
-# 添加 group 信息
+# add group information
 merged_seurat$group <- ifelse(grepl("^Stress", merged_seurat$sample), "CRS", "Control")
 
 save(merged_seurat, file = file.path(out_dir, "mdd_liver_doublet_removed_merged.RData"))#24883 features across 52848 samples
